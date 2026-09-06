bf64c149578adcbfbd7815bd6557893068a5a5b1
Teillon Duarte <teillon.duarte@outlook.com>
2026-09-06 18:39:07 -0300

    Service Layer, component registration

---
+++ src/main/java/com/teillonduarte/aulasspring/services/UserService.java
@@ -0,0 +1,27 @@
+package com.teillonduarte.aulasspring.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.teillonduarte.aulasspring.entities.User;
+import com.teillonduarte.aulasspring.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long Id) {
+		Optional<User> obj = repository.findById(Id);
+		return obj.get();
+	}
+
+}
