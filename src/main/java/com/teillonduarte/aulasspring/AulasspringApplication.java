55f0edef660364084f99b0995721ecb73a7deb7a
Teillon Duarte <teillon.duarte@outlook.com>
2026-09-06 14:58:17 -0300

    Project Created

---
+++ src/main/java/com/teillonduarte/aulasspring/AulasspringApplication.java
@@ -0,0 +1,13 @@
+package com.teillonduarte.aulasspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulasspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulasspringApplication.class, args);
+	}
+
+}
