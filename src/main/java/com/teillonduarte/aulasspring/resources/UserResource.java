68f974b3eed17a61cbd2cc704c0624a1b45ce199
Teillon Duarte <teillon.duarte@outlook.com>
2026-09-06 15:14:19 -0300

    User entity and resource

---
+++ src/main/java/com/teillonduarte/aulasspring/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.teillonduarte.aulasspring.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.teillonduarte.aulasspring.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Maria", "maria@gmail.com", "9999-5999", "123456");
+		return ResponseEntity.ok().body(u);
+	}
+
+}
