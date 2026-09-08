c7e3084a8715c1ad61408358dcad99553bb3c7e6
Teillon Duarte <teillon.duarte@outlook.com>
2026-09-08 18:04:45 -0300

    Payment One-to-One association

--- src/main/java/com/teillonduarte/aulasspring/config/TestConfig.java
+++ src/main/java/com/teillonduarte/aulasspring/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.teillonduarte.aulasspring.entities.Category;
 import com.teillonduarte.aulasspring.entities.Order;
 import com.teillonduarte.aulasspring.entities.OrderItem;
+import com.teillonduarte.aulasspring.entities.Payment;
 import com.teillonduarte.aulasspring.entities.Product;
 import com.teillonduarte.aulasspring.entities.User;
 import com.teillonduarte.aulasspring.entities.enums.OrderStatus;
@@ -80,6 +81,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 	 
 	
